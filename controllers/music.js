ea1bac82da4e6048d7cf8028af4d313e6b765eee
straf16 <depoksugutamu621@gmail.com>
2019-10-31 21:39:46 +0700

    get lyric musixmatch

--- controllers/music.js
+++ controllers/music.js
@@ -1,4 +1,5 @@
 const dezzerAxios = require('../config/apis/dezzerAPI')
+const lyricsAxios = require('../config/apis/lyricsAPI')
 
 class MusicController {
   static getTopMusics (req, res, next) {
@@ -30,6 +31,34 @@ class MusicController {
         res.status(200).json(data)
       })
       .catch(next)
+  }
+  static getLyric (req, res, next) {
+    const { artist, track } = req.query
+    lyricsAxios({
+      method: 'get',
+      url: `/track.search`,
+      params: {
+        apikey: process.env.MUSIXMATCH_SECRET,
+        q_artist: artist,
+        q_track: track
+      }
+    })
+      .then(({data}) => {
+        const track_id = data.message.body.track_list[0].track.track_id
+        return lyricsAxios({
+          method: 'get',
+          url: `/track.lyrics.get`,
+          params: {
+            apikey: process.env.MUSIXMATCH_SECRET,
+            track_id
+          }
+        })
+      })
+      .then(({data}) => {
+        res.status(200).json(data.message.body.lyrics)
+      })
+      .catch(next)
+
   }
 }
 
