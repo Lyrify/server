4f4be1b7ffe0779959bbbbda748186109bf9b42d
straf16 <depoksugutamu621@gmail.com>
2019-11-01 09:44:20 +0700

    resolve package

--- routes/translateRoute.js
+++ routes/translateRoute.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const TranslateController = require('../controllers/TranslateController.js');
 const JWT = require('../middlewares/JWT.js');
 
-router.get('/', JWT.authentication, JWT.authorization, TranslateController.translate);
-router.get('/detectlanguage', JWT.authentication, JWT.authorization, TranslateController.detectLanguage);
+// router.get('/', JWT.authentication, JWT.authorization, TranslateController.translate);
+// router.get('/detectlanguage', JWT.authentication, JWT.authorization, TranslateController.detectLanguage);
 
 module.exports = router;
